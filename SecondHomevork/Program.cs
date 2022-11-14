0eff51bfa6726a085a54a55af131bfcbb3561913
Ze1lb0ba <stankevich_dg@school629.ru>
2022-11-14 18:49:23 +0300

    Добавьте файлы проекта.

---
+++ SecondHomevork/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SecondHomevork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (string name, string secondName, int age, string[] petnames, string[] favColours) user;
+            user = UserInfo();
+
+        }
+
+        public static (string name, string secondName, int age, string[] petnames, string[] favColours) UserInfo()
+        {
+            Console.WriteLine("Введите имя");
+            
+        }
+    }
+}
